5ed9c458d994e8ab5913a35ade97b265105c2ed9
bekce <seb@sebworks.com>
2017-03-06 16:59:58 +0300

    Refactor TestContext into TestLifecycleBean

--- src/test/java/com/sebworks/automated/it/DemoIT.java
+++ src/test/java/com/sebworks/automated/it/DemoIT.java
@@ -9,24 +9,13 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.JUnitCore;
 import org.junit.runner.RunWith;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.event.ContextClosedEvent;
-import org.springframework.context.event.ContextRefreshedEvent;
-import org.springframework.context.event.EventListener;
-import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import com.github.raonifn.casperjs.junit.CasperEnvironment;
 import com.github.raonifn.casperjs.junit.CasperRunner;
-import com.sebworks.automated.it.entity.User;
-import com.sebworks.automated.it.repository.UserRepository;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ActiveProfiles("test")
@@ -37,48 +26,6 @@ public class DemoIT {
 	public void casperJS() throws Exception {
 		CasperIT.countDownLatch();
 		JUnitCore.runClasses(CasperIT.class);
-	}
-
-	@Configuration
-	public static class TestConfig extends DemoApplication {
-		private static final Logger logger = LoggerFactory.getLogger(TestConfig.class);
-		
-		@Bean
-		public TestBean testBean(){
-			return new TestBean();
-		}
-		
-		public static class TestBean {
-			@Autowired
-			private MongoTemplate mongoTemplate;
-			@Autowired
-			private UserRepository userRepository;
-
-			@EventListener
-			public void handleContextRefresh(ContextRefreshedEvent event) {
-				logger.info("Importing data...");
-				System.out.println("********************************");
-				System.out.println("************IMPORT**************");
-				System.out.println("********************************");
-
-				userRepository.save(new User("John", "Doe", "+901112223344"));
-				userRepository.save(new User("Jane", "Doe", "+909116667788"));
-			}
-
-			@EventListener
-			public void handleContextClose(ContextClosedEvent event) {
-				logger.info("Dropping database...");
-				
-				System.out.println("********************************");
-				System.out.println("*************DROP***************");
-				System.out.println("********************************");
-				if ("demo-test".equals(mongoTemplate.getDb().getName())) {
-					mongoTemplate.getDb().dropDatabase();
-					logger.info("Dropped database: demo-test");
-				}
-			}
-		}
-
 	}
 
 	@RunWith(CasperRunner.class)
---
+++ src/test/java/com/sebworks/automated/it/TestLifecycleBean.java
@@ -0,0 +1,49 @@
+package com.sebworks.automated.it;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.event.ContextClosedEvent;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.stereotype.Component;
+
+import com.sebworks.automated.it.entity.User;
+import com.sebworks.automated.it.repository.UserRepository;
+
+@Component
+public class TestLifecycleBean {
+
+	private static final Logger logger = LoggerFactory.getLogger(TestLifecycleBean.class);
+	
+	@Autowired
+	private MongoTemplate mongoTemplate;
+	@Autowired
+	private UserRepository userRepository;
+
+	@EventListener
+	public void handleContextRefresh(ContextRefreshedEvent event) {
+		logger.info("Importing data...");
+		System.out.println("********************************");
+		System.out.println("************IMPORT**************");
+		System.out.println("********************************");
+
+		userRepository.save(new User("John", "Doe", "+901112223344"));
+		userRepository.save(new User("Jane", "Doe", "+909116667788"));
+	}
+
+	@EventListener
+	public void handleContextClose(ContextClosedEvent event) {
+		logger.info("Dropping database...");
+		
+		System.out.println("********************************");
+		System.out.println("*************DROP***************");
+		System.out.println("********************************");
+		if ("demo-test".equals(mongoTemplate.getDb().getName())) {
+			mongoTemplate.getDb().dropDatabase();
+			logger.info("Dropped database: demo-test");
+		}
+	}
+
+}
