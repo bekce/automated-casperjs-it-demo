c385ab856a0a1c8871627f0be77277de40ac0835
bekce <seb@sebworks.com>
2017-03-06 15:54:09 +0300

    First

---
+++ src/main/java/com/sebworks/AutomatedCasperjsItDemoApplication.java
@@ -0,0 +1,12 @@
+package com.sebworks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutomatedCasperjsItDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutomatedCasperjsItDemoApplication.class, args);
+	}
+}
